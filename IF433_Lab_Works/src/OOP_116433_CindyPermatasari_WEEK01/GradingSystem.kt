24260e34b3e6b54a9a306a9378a2d26eb0806511
cindy <cindykhoe06@gmail.com>
2026-02-06 15:44:48 +0700

    week01: refactor to use val and string templates

--- IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GradingSystem.kt
+++ IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GradingSystem.kt
@@ -1,4 +1,10 @@
 package OOP_116433_CindyPermatasari_WEEK01
 
-class GradingSystem {
+fun main() {
+    //Refactor: gunakan val dan hapus tipe data eksplisit
+    val name: = "John Thor"
+    val score: = 80
+
+    //Refactor: string template ($name)
+    println("Nama:" + name +", Nilai: " + score)
 }
