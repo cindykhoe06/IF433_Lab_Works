ae80a2deda5dfbaa589aa0044b63fdb171610230
cindy <cindykhoe06@gmail.com>
2026-02-07 23:37:34 +0700

    week01: (tugas 2) implement receipt printing

--- IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GameStore.kt
+++ IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GameStore.kt
@@ -8,3 +8,9 @@ fun calculateDiscount(price: Int): Int = if (price > 500000) {
 } else {
     (price * 10) / 100
 }
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== Struk Pembelian ===")
+    println("Judul Game : $title")
+    println("Harga Akhir: Rp$finalPrice")
+}
