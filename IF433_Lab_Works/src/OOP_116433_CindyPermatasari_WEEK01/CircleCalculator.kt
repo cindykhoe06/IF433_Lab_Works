251e42e0a0e31422e3c886eb3f7491b8bd3e0b85
cindy <cindykhoe06@gmail.com>
2026-02-07 23:23:32 +0700

    clean up unused argos and comments

--- IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/CircleCalculator.kt
+++ IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/CircleCalculator.kt
@@ -1,23 +1,19 @@
 package OOP_116433_CindyPermatasari_WEEK01
 
-fun main(args: Array<String>) {
-    //variable definition
+fun main() {
     val radius  = 7.0
     val pi  = 3.14
 
-    //calculation
     var area = pi * radius * radius
 
-    //output concatenation
     println("Radius: $radius")
-
-    //logic check
-    checkSize(area)
     println("Check Size: ${checkSize(area)}")
 }
 
-fun checkSize(area: Double) { if (area > 100)
-    "This is a Big Circle"
-     else
-        "This is a Small Circle"
+fun checkSize(area: Double): String {
+    if (area > 100) {
+        return "This is a Big Circle"
+    } else {
+        return "This is a Small Circle"
+    }
 }
