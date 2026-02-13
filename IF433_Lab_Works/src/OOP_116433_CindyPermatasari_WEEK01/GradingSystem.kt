ec5a3cadb0fd8b59363105b971d363788381cf21
cindy <cindykhoe06@gmail.com>
2026-02-13 16:00:25 +0700

    week01: pembetulan file grading system

--- IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GradingSystem.kt
+++ IF433_Lab_Works/src/OOP_116433_CindyPermatasari_WEEK01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package OOP_116433_CindyPermatasari_WEEK01
 
-fun main() {
+fun GradingSystem() {
     //Refactor: gunakan val dan hapus tipe data eksplisit
-    val name: = "John Thor"
-    val score: = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
     //Refactor: string template ($name)
     println("Nama:" + name +", Nilai: " + score)
