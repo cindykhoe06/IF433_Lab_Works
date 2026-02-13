02ab528918a535f52ca418b8eef5ac1a53531a21
cindy <cindykhoe06@gmail.com>
2026-02-13 15:21:37 +0700

    week02: add warning validation in init block

--- IF433_Lab_Works/src/OOP_116433_Week02/student.kt
+++ IF433_Lab_Works/src/OOP_116433_Week02/student.kt
@@ -7,3 +7,15 @@ class student (
 ){
     //body class kosong dulu
 }
+
+class student (val name: String, val nim: String, val major: String) {
+    init {
+        //validasi sederhana : cek panjang nim
+        if (nim.length != 5) {
+            println("WARNING: obejk tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: objek student $name berhasil dialokasikan di memory.")
+        }
+    }
+}
