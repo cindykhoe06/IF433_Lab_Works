d836482ebf61900e3852d9eece7df4ed284f7e05
cindy <cindykhoe06@gmail.com>
2026-02-20 14:06:59 +0700

    week03: (task 1) implement weapon testing in main

--- IF433_Lab_Works/src/OOP_116433_Week03/main.kt
+++ IF433_Lab_Works/src/OOP_116433_Week03/main.kt
@@ -7,4 +7,20 @@ fun main() {
     println("gaji: ${e.salary}")
     e.increasePerfomationRating()
     println("pajak yang harus dibayar: ${e.tax}")
+
+    println("TUGAS 1: UJI COBA WEAPON")
+
+    // 1. Buat objek Weapon
+    val pedang = Weapon("Excalibur")
+
+    // 2. Coba set damage ke -50 (harus gagal/tetap nilai default)
+    pedang.damage = -50
+    println("Damage setelah di-set -50: ${pedang.damage}")
+
+    // 3. Coba set damage ke 9999 (harus dipaksa jadi 1000)
+    pedang.damage = 9999
+    println("Damage setelah di-set 9999: ${pedang.damage}")
+
+    // 4. Print Tier-nya
+    println("Tier Senjata: ${pedang.tier}")
 }
