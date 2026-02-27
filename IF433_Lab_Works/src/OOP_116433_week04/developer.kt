5c97674ef24ba3c435c768a7334ad9d88dfb0eab
cindykhoe06 <cindykhoe06@gmail.com>
2026-02-27 16:25:21 +0700

    week04: (task 2) create developer class

---
+++ IF433_Lab_Works/src/OOP_116433_week04/developer.kt
@@ -0,0 +1,14 @@
+package OOP_116433_week04
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+
+    // calculateBonus() tidak perlu di-override karena mengikuti bawaan Parent
+}
