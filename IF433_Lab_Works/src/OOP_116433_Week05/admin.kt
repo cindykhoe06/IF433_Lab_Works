b52be6c6dced6ae2dbda0ac4ad4e03d52a47b26e
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-06 14:34:14 +0700

    week05: create adminclass inheriting pegawai

---
+++ IF433_Lab_Works/src/OOP_116433_Week05/admin.kt
@@ -0,0 +1,12 @@
+package OOP_116433_Week05
+
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    //fungsi unik/spesifik yang hanya dimiliki admin
+    fun doAdminWork() {
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
