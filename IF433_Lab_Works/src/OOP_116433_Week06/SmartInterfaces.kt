e127180ca412a09167f4aeb174f2a961b06da6c5
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-13 15:24:56 +0700

    week06: (task) create smart home interfaces

---
+++ IF433_Lab_Works/src/OOP_116433_Week06/SmartInterfaces.kt
@@ -0,0 +1,20 @@
+package OOP_116433_Week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+
+    // Default function
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
