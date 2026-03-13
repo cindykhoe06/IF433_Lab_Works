f496fb570b5d087187c16ef0b0bc768c2c1b5ff3
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-13 14:53:24 +0700

    week06: create compound interfaces

---
+++ IF433_Lab_Works/src/OOP_116433_Week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package OOP_116433_Week06
+
+//interface = Cand-Do (behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
