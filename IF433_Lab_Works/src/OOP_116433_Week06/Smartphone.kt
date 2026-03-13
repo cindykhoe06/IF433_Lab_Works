5eca1f7d6227ab176eaf300ab413f49c6764619b
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-13 15:09:32 +0700

    week06: fix diamond problem using explicit super delegation

--- IF433_Lab_Works/src/OOP_116433_Week06/Smartphone.kt
+++ IF433_Lab_Works/src/OOP_116433_Week06/Smartphone.kt
@@ -2,5 +2,10 @@ package OOP_116433_Week06
 
 //error: class smartphone inherits multiple implementations of turnOn()
 class Smartphone : Camera, Phone {
-
+    //manually override to resolve ambiguity
+    override fun turnOn() {
+        super<Phone>.turnOn() //menjalankan logika camera
+        super<Phone>.turnOn() //menjalankan logika phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
