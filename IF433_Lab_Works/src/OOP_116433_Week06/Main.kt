b3da3a874fa24b846595c8c2448b6861f84509bc
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-13 15:20:40 +0700

    week06: implement decoupled processCheckout in main

---
+++ IF433_Lab_Works/src/OOP_116433_Week06/Main.kt
@@ -0,0 +1,22 @@
+package OOP_116433_Week06
+
+//fungsi ini decoupled! tidak peduli kelas aslinya apa.
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("-> memulai checkout...")
+    method.pay(amount) //dynamic polymorphism in action
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== Testing Checkout ===")
+    processCheckout(pay1, 50000.0)
+    processCheckout(pay2, 150000.0)
+}
