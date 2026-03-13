5a39ada60d0ff4c08f7b1bfd18c3e6633fcd7cfc
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-13 14:49:45 +0700

    week06: create button class implementing Clickable

--- IF433_Lab_Works/src/OOP_116433_Week06/Clickable.kt
+++ IF433_Lab_Works/src/OOP_116433_Week06/Clickable.kt
@@ -2,3 +2,9 @@ interface Clickable {
     val name: String // abstract property, tidak ada nilai default
     fun click()
 }
+
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
+}
