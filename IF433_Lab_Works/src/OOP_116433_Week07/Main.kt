1a89fa3afe688b28ec9e9ef26324eb36b2f2c353
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-27 15:44:38 +0700

    week07: (task) test GameManager singleton in main

--- IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
+++ IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
@@ -1,40 +1,10 @@
 package OOP_116433_Week07
 
 fun main() {
-    println("=== TEST SINGLETON ====")
-    println("Status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
-
-    println("\n === TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient() // instansiasi lewat factory
-    client.connect()
-
-    println("\n === TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1) //akan mencetak memori hash
-    println("Sama? ${reg1 == reg2}") //false
-
-    println("\n=== TEST DATA CLASS ===")
-    val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-    println(data1) //otomatis readable format
-    println("Sama? ${data1 == data2}") //True (structural equality)
-
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-
-    val (userName, userAge) = data1 // Destructuring Declaration
-    println("Destructured: $userName berumur $userAge")
-
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    //ERROR: 'when' expression must be exhaustive
-    val uiMessage = when(response) {
-        is ApiResponse.Loading -> "Tampilkan Spinner"
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    fun main() {
+        println("=== TEST SINGLETON ====")
+        println("Status: ${DatabaseManager.connectionStatus}")
+        DatabaseManager.connect()
+        DatabaseManager.connect()
     }
-    println(uiMessage)
 }
