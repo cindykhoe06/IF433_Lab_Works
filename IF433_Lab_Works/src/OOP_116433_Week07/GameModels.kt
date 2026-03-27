6352caacee4c46031c616d92f925de7efaffbf7b
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-27 15:30:44 +0700

    week07: perbaikan checkpoint 11

---
+++ IF433_Lab_Works/src/OOP_116433_Week07/GameModels.kt
@@ -0,0 +1,15 @@
+package OOP_116433_Week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
--- IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
+++ IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
@@ -32,7 +32,9 @@ fun main() {
 
     //ERROR: 'when' expression must be exhaustive
     val uiMessage = when(response) {
+        is ApiResponse.Loading -> "Tampilkan Spinner"
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
     }
+    println(uiMessage)
 }
