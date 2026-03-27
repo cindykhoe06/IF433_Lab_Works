c32be9c4c439292b4691a2dc4d6fd5d8f32dbf44
cindykhoe06 <cindykhoe06@gmail.com>
2026-03-27 14:55:09 +0700

    week07: test Singleton and Companion Object in main

--- IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
+++ IF433_Lab_Works/src/OOP_116433_Week07/Main.kt
@@ -1,2 +1,11 @@
 package OOP_116433_Week07
 
+fun main() {
+    println("=== TEST SINGLETON ====")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n === TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() // instansiasi lewat factory
+    client.connect()
+}
