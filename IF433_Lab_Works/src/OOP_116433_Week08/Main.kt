39777b6121826518b9fe17f114ce2799b7940c43
cindykhoe06 <cindykhoe06@gmail.com>
2026-04-17 15:35:04 +0700

    week08: implement safe casting as? to filter collection

--- IF433_Lab_Works/src/OOP_116433_Week08/Main.kt
+++ IF433_Lab_Works/src/OOP_116433_Week08/Main.kt
@@ -1,17 +1,17 @@
 package OOP_116433_Week08
 
-fun main(){
+fun main() {
     println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
 
     //rantai safe calls yang elegan
-    val destination = emptyOrder.deliveryDetails?.address?.city?.name?:"Kota tidak diketahui"
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota tidak diketahui"
     println("Tujuan pengiriman: $destination")
 
     println("\n===TEST LET BLOCK====")
     val validOrder = Order(null, 250000)
 
-    val receipt = validOrder.totalPrice?.let {price ->
+    val receipt = validOrder.totalPrice?.let { price ->
         //blok ini hanya jalan jika totalPrice tidak null
         val tax = price * 0.11
         "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
@@ -19,11 +19,19 @@ fun main(){
     println(receipt)
 
     println("\n=== TEST SAFE CASTING ===")
-    val mixedData: List<Any> = ListOf(
+    val mixedData: List<Any> = listOf(
         "Smartphone",
         1500000,
-        userProfile("Andi", null),
+        UserProfile("Andi", null),
         "Laptop",
         4500000.0
     )
+    for (item in mixedData) {
+        val text = item as? String
+
+        // hanya jalan kalau berhasil cast ke String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
