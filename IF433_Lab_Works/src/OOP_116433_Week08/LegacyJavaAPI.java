6ca27607a5186f475d5f3254d950edebd8d5659d
cindykhoe06 <cindykhoe06@gmail.com>
2026-04-17 15:47:58 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ IF433_Lab_Works/src/OOP_116433_Week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package OOP_116433_Week08;
+
+public class LegacyJavaAPI {
+    //fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
