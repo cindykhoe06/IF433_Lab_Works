5b20818e2d731ebe7c06ffa8b05adfb5c4445e87
cindykhoe06 <cindykhoe06@gmail.com>
2026-04-17 15:45:54 +0700

    week08: implement requireNotNull as safer alternative

--- IF433_Lab_Works/src/OOP_116433_Week08/NotificationService.kt
+++ IF433_Lab_Works/src/OOP_116433_Week08/NotificationService.kt
@@ -23,5 +23,17 @@ class NotificationService {
         } catch (e: NullPointerException) {
             println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
         }
+        println("\n=== ALTERNATIF AMAN (requireNotNull) ===")
+
+        val apiResponse: Map<String, String?> =
+            mapOf("status" to "200", "token" to null)
+
+        try {
+            val token = requireNotNull(apiResponse["token"]) {
+                "CRITICAL EXCEPTION: Token otentikasi tidak ditemukan dari server!"
+            }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 }
