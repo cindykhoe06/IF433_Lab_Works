ed30046d3d8cba6e8c6b7e11ef676189235cb2d5
cindykhoe06 <cindykhoe06@gmail.com>
2026-04-24 15:58:57 +0700

    week09: (task) populate mock trade history list

---
+++ IF433_Lab_Works/src/OOP_116433_Week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package OOP_116433_Week09
+
+fun main() {
+    // 2. Inisialisasi Data Uji
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 2.0, "OPEN"), // Status OPEN (akan difilter)
+        TradeLog("BTCUSDT", "SHORT", 50, -25.0, "CLOSED"),
+        TradeLog("BNBUSDT", "LONG", 15, 10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 10, -1.5, "OPEN")  // Status OPEN (akan difilter)
+    )
