c53d205405c7ffcd1937b654bcb2b0032641842a
cindykhoe06 <cindykhoe06@gmail.com>
2026-04-24 16:03:40 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- IF433_Lab_Works/src/OOP_116433_Week09/TradingDashboard.kt
+++ IF433_Lab_Works/src/OOP_116433_Week09/TradingDashboard.kt
@@ -20,4 +20,6 @@ fun main() {
     val worstPerformersString = losingTrades
         .sortedBy { it.roe } // Diurutkan dari minus terbesar ke terkecil
         .map { "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
+    val uniquePairs = tradeHistory.map { it.pair }.toSet()
 }
