a0db371a8f2448000610f7ca9bb2a2edf5b98f0f
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-07 22:26:07 +0700

    week10: (task) initialize coin repository

---
+++ IF433_Lab_Works/src/OOP_116433_Week10/CryptoDashboard.kt
@@ -0,0 +1,9 @@
+package OOP_116433_Week10
+
+fun main() {
+    // CHECKPOINT 5: Initialize coin repository
+    val coinRepo = WalletRepository<Coin>()
+
+    println("=== Crypto Dashboard ===")
+    println("Coin repository initialized.")
+}
