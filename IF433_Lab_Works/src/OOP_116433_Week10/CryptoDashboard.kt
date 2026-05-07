8bb8d5989d3f2f0239fa3b0ea7db448f962ae234
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-07 22:26:54 +0700

    week10: (task) populate coin repository

--- IF433_Lab_Works/src/OOP_116433_Week10/CryptoDashboard.kt
+++ IF433_Lab_Works/src/OOP_116433_Week10/CryptoDashboard.kt
@@ -1,9 +1,12 @@
 package OOP_116433_Week10
 
 fun main() {
-    // CHECKPOINT 5: Initialize coin repository
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.3))
+    coinRepo.add(Coin("USDT", 100.0))
+
     println("=== Crypto Dashboard ===")
-    println("Coin repository initialized.")
+    println("Coin repository populated with BTC, ETH, USDT.")
 }
