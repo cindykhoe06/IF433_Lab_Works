42d2834ad682ccfefdbfb28490083707f8c7a91d
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-07 22:16:34 +0700

    perbaikan error

--- IF433_Lab_Works/src/OOP_116433_Week10/GenericMethods.kt
+++ IF433_Lab_Works/src/OOP_116433_Week10/GenericMethods.kt
@@ -3,7 +3,7 @@ package OOP_116433_Week10
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 
-    fun <T> processData(input: T): T {
-        return input
     }
+    fun <T> processData(input: T): T {
+    return input
 }
