4123ce79d89b1a529a1eac8adc1f53af1c694a2b
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-07 21:54:46 +0700

    week10: create generic Box class

---
+++ IF433_Lab_Works/src/OOP_116433_Week10/GenericBox.kt
@@ -0,0 +1,4 @@
+package OOP_116433_Week10
+
+// Kelas generic menggunakan <T> sebagai placeholder tipe data
+class Box<T>(var value: T)
---
+++ IF433_Lab_Works/src/OOP_116433_Week11/StringExtensions.kt
@@ -0,0 +1,10 @@
+package OOP_116433_Week11
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
