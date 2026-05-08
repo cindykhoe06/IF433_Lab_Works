0a20cddf87d25156dd699a34b9ea0e170a3f9a41
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-08 15:17:18 +0700

    week11: [Task] create SmartDevice data class

---
+++ IF433_Lab_Works/src/OOP_116433_Week11/SmartDevice.kt
@@ -0,0 +1,9 @@
+package OOP_116433_Week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var id: Int = 0,
+    var powerLoad: Int = 0
+)
