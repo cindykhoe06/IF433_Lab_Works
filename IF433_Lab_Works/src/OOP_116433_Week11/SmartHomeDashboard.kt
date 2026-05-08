c852e91108cd53e795530128af54be83e9c17e48
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-08 15:20:43 +0700

    week11: [Task] configure Philips W2 using apply

--- IF433_Lab_Works/src/OOP_116433_Week11/SmartHomeDashboard.kt
+++ IF433_Lab_Works/src/OOP_116433_Week11/SmartHomeDashboard.kt
@@ -2,4 +2,12 @@ package OOP_116433_Week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice("Philips W2 Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+        println("[Setup] Lampu ${it.name} ditambahkan.")
+    }
 }
