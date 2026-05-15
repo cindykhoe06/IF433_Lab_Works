c4700c97d5bbe8a10fcf6b5a3be1b1c1de16ca93
cindykhoe06 <cindykhoe06@gmail.com>
2026-05-15 15:00:48 +0700

    perbaikan error

--- IF433_Lab_Works/src/OOP_116433_Week12/SmartFeeder.kt
+++ IF433_Lab_Works/src/OOP_116433_Week12/SmartFeeder.kt
@@ -2,9 +2,6 @@ package OOP_116433_Week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-}
-
-fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
     if (isJammed) throw DispenserJamException()
+    return 0
 }
