47993972bf242f56368ca0c4de17de6400e78e63
Cindy Permatasari Khoeniawan <cindypermatasarikhoeniawan@Cindys-MacBook-Air.local>
2026-05-22 14:43:50 +0700

    week13: implement basic writeText function

---
+++ IF433_Lab_Works/src/OOP_116433_Week13/BasicFileIO.kt
@@ -0,0 +1,9 @@
+package OOP_116433_Week13
+import java.io.File
+
+fun main() {
+    println("=== TEST WRITE TEXT ===")
+    val file = File("notes.txt")
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+    println("File berhasil dibuat dan ditulis.")
+}
