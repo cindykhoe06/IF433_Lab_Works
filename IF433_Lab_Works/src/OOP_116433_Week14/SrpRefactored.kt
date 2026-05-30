bd70dc75a9993a2ceb27fd247f521a90e6577b24
Cindy Permatasari Khoeniawan <cindypermatasarikhoeniawan@Cindys-MacBook-Air.local>
2026-05-30 17:52:44 +0700

    week14: extract UserValidator and UserRepository

---
+++ IF433_Lab_Works/src/OOP_116433_Week14/SrpRefactored.kt
@@ -0,0 +1,14 @@
+package OOP_116433_Week14
+
+data class User(val name: String, val email: String, val age: Int)
+
+class UserValidator {
+    fun validate(user: User): Boolean =
+        user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
