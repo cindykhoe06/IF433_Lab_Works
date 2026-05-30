07c2f32825df9851b541d43f0420cfa4c8a62cb6
Cindy Permatasari Khoeniawan <cindypermatasarikhoeniawan@Cindys-MacBook-Air.local>
2026-05-30 17:51:21 +0700

    week13: implement basic writeText function

--- IF433_Lab_Works/src/OOP_116433_Week13/ResourceHandling.kt
+++ IF433_Lab_Works/src/OOP_116433_Week13/ResourceHandling.kt
@@ -3,7 +3,7 @@ import java.io.File
 
 fun main() {
     println("=== TEST UNSAFE RESOURCE HANDLING ===")
-    val unsafeFile = File(pathname = "unsafe_logs.txt")
+    val unsafeFile = File("unsafe_logs.txt")
     val writer = unsafeFile.printWriter()
     writer.println("Log 1: Membuka koneksi database...")
     writer.println("Log 2: Menulis data pengguna...")
